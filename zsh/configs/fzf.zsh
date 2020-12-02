70d4d62f409126e52b750bbd7451e6cb89ed6c04
Pawel Borak <borak.pawel@gmail.com>
2020-12-02 12:20:30 +0100

    Color change no. 2077

--- zsh/configs/fzf.zsh
+++ zsh/configs/fzf.zsh
@@ -5,8 +5,8 @@ export FZF_DEFAULT_OPTS='
   --height 75% --multi --reverse --margin=0,1
   --bind ctrl-f:preview-page-down,ctrl-b:preview-page-up
   --color bg+:#262626,fg+:#dadada,hl:#ae81ff,hl+:#ae81ff
-  --color border:#303030,info:#cfcfb0,header:#80a0ff,spinner:#36c692
-  --color prompt:#87afff,pointer:#ff5189,marker:#f09479
+  --color border:#303030,info:#cfcfb0,header:#82aaff,spinner:#21c7a8
+  --color prompt:#87afff,pointer:#ff5874,marker:#7c8f8f
 '
 export FZF_CTRL_T_OPTS="--preview 'bat --color=always --line-range :500 {}'"
 
