5fd88fa7eb33572d038c3acea416af1af98a984b
Pawel Borak <borak.pawel@gmail.com>
2021-07-26 12:12:33 +0200

    Remove custom c-w in zsh

--- zsh/configs/keybindings.zsh
+++ zsh/configs/keybindings.zsh
@@ -1,9 +1,4 @@
 # handy keybindings
-backward-kill-dir () {
-    local WORDCHARS=${WORDCHARS/\/}
-    zle backward-kill-word
-}
-zle -N backward-kill-dir
 
 bindkey "^a" beginning-of-line
 bindkey "^e" end-of-line
@@ -14,7 +9,6 @@ bindkey "^d" delete-char
 bindkey "^p" history-search-backward
 bindkey "^n" history-search-forward
 bindkey "^y" accept-and-hold
-bindkey "^w" backward-kill-dir
 bindkey "^u" backward-kill-line
 bindkey "^g" autosuggest-accept
 
