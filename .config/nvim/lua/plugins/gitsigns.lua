da014ac23af75bdcbdb9f149a95a1ff5fc53c3a6
Pawel Borak <borak.pawel@gmail.com>
2022-02-02 11:14:24 +0100

    refactor(gitsigns): adjust keymaps

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -10,31 +10,21 @@ return function()
       changedelete = { hl = 'GitSignsChange', text = '▌' },
     },
     on_attach = function()
-      gh.nnoremap(
-        ']g',
-        "&diff ? ']c' : '<cmd>lua require\"gitsigns\".next_hunk()<CR>'",
-        { expr = true }
-      )
-      gh.nnoremap(
-        '[g',
-        "&diff ? '[c' : '<cmd>lua require\"gitsigns\".prev_hunk()<CR>'",
-        { expr = true }
-      )
-      gh.nnoremap('<leader>gw', '<cmd>lua require"gitsigns".stage_buffer()<CR>')
-      gh.nnoremap('<leader>gr', '<cmd>lua require"gitsigns".reset_buffer()<CR>')
-      gh.nnoremap('<leader>hs', '<cmd>lua require"gitsigns".stage_hunk()<CR>')
-      gh.vnoremap(
-        '<leader>hs',
-        '<cmd>lua require"gitsigns".stage_hunk({vim.fn.line("."), vim.fn.line("v")})<CR>'
-      )
-      gh.nnoremap('<leader>hu', '<cmd>lua require"gitsigns".undo_stage_hunk()<CR>')
-      gh.nnoremap('<leader>hr', '<cmd>lua require"gitsigns".reset_hunk()<CR>')
-      gh.vnoremap(
-        '<leader>hr',
-        '<cmd>lua require"gitsigns".reset_hunk({vim.fn.line("."), vim.fn.line("v")})<CR>'
-      )
-      gh.nnoremap('<leader>hp', '<cmd>lua require"gitsigns".preview_hunk()<CR>')
-      gh.nnoremap('<leader>hl', '<cmd>lua require"gitsigns".setqflist("all")<CR>')
+      local gs = package.loaded.gitsigns
+
+      gh.nnoremap(']g', "&diff ? ']c' : '<cmd>Gitsigns next_hunk<CR>'", { expr = true })
+      gh.nnoremap('[g', "&diff ? '[c' : '<cmd>Gitsigns prev_hunk<CR>'", { expr = true })
+      gh.nnoremap('<leader>gw', gs.stage_buffer)
+      gh.nnoremap('<leader>gr', gs.reset_buffer)
+      gh.nnoremap('<leader>hs', ':Gitsigns stage_hunk<CR>')
+      gh.vnoremap('<leader>hs', ':Gitsigns stage_hunk<CR>')
+      gh.nnoremap('<leader>hu', gs.undo_stage_hunk)
+      gh.nnoremap('<leader>hr', ':Gitsigns reset_hunk<CR>')
+      gh.vnoremap('<leader>hr', ':Gitsigns reset_hunk<CR>')
+      gh.nnoremap('<leader>hp', gs.preview_hunk)
+      gh.nnoremap('<leader>hl', function()
+        gs.setqflist 'all'
+      end)
     end,
   }
 end
