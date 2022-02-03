5ceb401b89402e74ac47dde6d86c0c234aebb90f
Pawel Borak <borak.pawel@gmail.com>
2022-02-03 09:56:59 +0100

    chore(nvim): disable matchit

--- .config/nvim/lua/settings.lua
+++ .config/nvim/lua/settings.lua
@@ -20,6 +20,7 @@ vim.g.loaded_netrwPlugin = 0
 vim.g.loaded_netrwSettings = 0
 vim.g.loaded_netrwFileHandlers = 0
 vim.g.loaded_ruby_provider = 0
+vim.g.loaded_matchit = 0
 --------------------------------------------------------------------------------
 -- Message output on vim actions
 --------------------------------------------------------------------------------
