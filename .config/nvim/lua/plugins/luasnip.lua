fc5db1dbcacfa26c73be4f09667bc1f2a03087b0
Pawel Borak <borak.pawel@gmail.com>
2022-03-17 13:00:35 +0100

    fix(luasnip): change updateevents

--- .config/nvim/lua/plugins/luasnip.lua
+++ .config/nvim/lua/plugins/luasnip.lua
@@ -121,7 +121,7 @@ return function()
 
   ls.config.set_config {
     history = false,
-    updateevents = 'TextChanged,TextChangedI',
+    updateevents = 'TextChangedI',
     region_check_events = 'CursorMoved,CursorHold,InsertEnter',
     delete_check_events = 'InsertLeave',
     ext_opts = {
