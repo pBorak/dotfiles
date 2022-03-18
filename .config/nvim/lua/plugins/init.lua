c24c9e6e1f30469fc2f6d90cbb92d6905e7e765e
Pawel Borak <borak.pawel@gmail.com>
2022-03-18 09:01:41 +0100

    feat(dressing): pass telescope theme in the new way

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -253,8 +253,13 @@ packer.startup {
       config = function()
         require('dressing').setup {
           select = {
-            telescope = {
-              theme = 'cursor',
+            telescope = require('telescope.themes').get_cursor {
+              layout_config = {
+                height = function(self, _, max_lines)
+                  local results = #self.finder.results
+                  return (results <= max_lines and results or max_lines - 10) + 4 -- 4 is the size of the window
+                end,
+              },
             },
           },
         }
