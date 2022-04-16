f520800841119dbde1faa37091c7077c7dbe54cc
Pawel Borak <borak.pawel@gmail.com>
2022-04-16 13:04:28 +0200

    chore(indentline): tweak config

--- .config/nvim/lua/plugins/indent-blankline.lua
+++ .config/nvim/lua/plugins/indent-blankline.lua
@@ -1,10 +1,8 @@
 return function()
   require('indent_blankline').setup {
     char = '│',
-    show_foldtext = false,
+    context_char = '┃',
     show_current_context = true,
-    show_current_context_start = true,
-    show_current_context_start_on_current_line = false,
     filetype_exclude = {
       'log',
       'fugitive',
