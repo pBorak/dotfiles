38da2994cadf1fb57d01d6d5e074e7de0b2e0e1b
Pawel Borak <borak.pawel@gmail.com>
2022-05-24 09:08:51 +0200

    chore: remove todo-comments

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -265,16 +265,6 @@ packer.startup {
         }
       end,
     }
-    use {
-      'folke/todo-comments.nvim',
-      requires = 'nvim-lua/plenary.nvim',
-      config = function()
-        if vim.g.packer_compiled_loaded then
-          return
-        end
-        require('todo-comments').setup {}
-      end,
-    }
     ----------------------------------------------------------------------------
     -- Syntax
     ----------------------------------------------------------------------------
