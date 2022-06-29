a63d236b15ee9eea42e4ac5cccd0077ddb5c5208
Pawel Borak <borak.pawel@gmail.com>
2022-06-29 09:49:45 +0200

    fix(treesitter-context): remove unnecessary setup, tweak hl

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -277,7 +277,7 @@ packer.startup {
     use {
       'nvim-treesitter/nvim-treesitter-context',
       config = function()
-        require('treesitter-context').setup()
+        vim.api.nvim_set_hl(0, 'TreesitterContext', { link = 'Folded' })
       end,
     }
     ----------------------------------------------------------------------------
