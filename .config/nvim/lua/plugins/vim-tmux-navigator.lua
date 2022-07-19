25811c1089ad8b6abf7685f660d7109baff6ec58
Pawel Borak <borak.pawel@gmail.com>
2022-07-19 15:19:25 +0200

    fix(tmux_navigator): disable tmux_navigator_save_on_switch

--- .config/nvim/lua/plugins/vim-tmux-navigator.lua
+++ .config/nvim/lua/plugins/vim-tmux-navigator.lua
@@ -7,5 +7,4 @@ return function()
   -- Disable tmux navigator when zooming the Vim pane
   vim.g.tmux_navigator_disable_when_zoomed = 1
   vim.g.tmux_navigator_preserve_zoom = 1
-  vim.g.tmux_navigator_save_on_switch = 2
 end
