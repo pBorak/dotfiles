3a8f7dcabeecf50f6df7fbfc5c253bfca1d23e95
Pawel Borak <borak.pawel@gmail.com>
2022-07-23 12:27:02 +0200

    chore(autocommands): cursorline exclude toggleterm

--- .config/nvim/plugin/autocommands.lua
+++ .config/nvim/plugin/autocommands.lua
@@ -146,20 +146,23 @@ gh.augroup('QuickfixBehaviours', {
   },
 })
 
-local function should_show_cursorline()
-  return vim.bo.buftype ~= 'terminal'
+local cursorline_exclude = { 'toggleterm' }
+
+---@param buf number
+---@return boolean
+local function should_show_cursorline(buf)
+  return vim.bo[buf].buftype ~= 'terminal'
     and not vim.wo.previewwindow
     and vim.wo.winhighlight == ''
-    and vim.bo.filetype ~= ''
+    and vim.bo[buf].filetype ~= ''
+    and not vim.tbl_contains(cursorline_exclude, vim.bo[buf].filetype)
 end
 
 gh.augroup('Cursorline', {
   {
     event = { 'BufEnter' },
     pattern = { '*' },
-    command = function()
-      if should_show_cursorline() then vim.wo.cursorline = true end
-    end,
+    command = function(args) vim.wo.cursorline = should_show_cursorline(args.buf) end,
   },
   {
     event = { 'BufLeave' },
