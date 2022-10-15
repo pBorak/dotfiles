3c303d3aa0929208440f41550cb9f453dcfc0546
Pawel Borak <borak.pawel@gmail.com>
2022-10-15 21:29:36 +0200

    chore(nvim): use official toggleterm strategy for vim-test

--- .config/nvim/lua/plugins/vim-test.lua
+++ .config/nvim/lua/plugins/vim-test.lua
@@ -1,18 +1,5 @@
 return function()
-  local tt = require('toggleterm')
-  local ttt = require('toggleterm.terminal')
-
-  vim.g['test#custom_strategies'] = {
-    tterm = function(cmd) tt.exec(cmd) end,
-
-    tterm_close = function(cmd)
-      local term_id = 0
-      tt.exec(cmd, term_id)
-      ttt.get_or_create_term(term_id):close()
-    end,
-  }
-
-  vim.g['test#strategy'] = 'tterm_close'
+  vim.g['test#strategy'] = 'toggleterm'
   gh.nnoremap('<leader>tf', '<cmd>TestFile<CR>')
   gh.nnoremap('<leader>tl', '<cmd>TestLast<CR>')
   gh.nnoremap('<leader>tt', '<cmd>TestNearest<CR>')
