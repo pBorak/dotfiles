174a9cd9e4e1d81dd98629d72ba413e4f3027b63
Pawel Borak <borak.pawel@gmail.com>
2022-11-28 21:50:58 +0100

    fix: correct useState snippet

--- .config/nvim/luasnippets/javascript.lua
+++ .config/nvim/luasnippets/javascript.lua
@@ -7,7 +7,7 @@ return {
       name = 'React useState',
       docstring = { 'const [value, setValue] = React.useState("defaultValue")' },
     },
-    fmt('const [{}, set{setter}] = useState({})', {
+    fmt('const [{}, set{setter}] = React.useState({})', {
       i(1, 'value'),
       i(2, 'initialValue'),
       setter = l(l._1:gsub('^%l', string.upper), 1),
