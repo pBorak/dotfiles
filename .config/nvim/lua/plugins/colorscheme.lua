a833e028f051e235fdf7236745f040483ee18085
Pawel Borak <borak.pawel@gmail.com>
2023-01-31 17:18:09 +0100

    feat(colorscheme): make tokyonight background all black

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -17,16 +17,11 @@ return {
           'fugitive',
           'undotree',
         },
-        on_highlights = function(hl, c)
-          hl.CursorLineNr = { fg = c.orange, bold = true }
-          local prompt = '#2d3149'
-          hl.TelescopeNormal = { bg = c.bg_dark, fg = c.fg_dark }
-          hl.TelescopeBorder = { bg = c.bg_dark, fg = c.bg_dark }
-          hl.TelescopePromptNormal = { bg = prompt }
-          hl.TelescopePromptBorder = { bg = prompt, fg = prompt }
-          hl.TelescopePromptTitle = { bg = c.fg_gutter, fg = c.orange }
-          hl.TelescopePreviewTitle = { bg = c.bg_dark, fg = c.bg_dark }
-          hl.TelescopeResultsTitle = { bg = c.bg_dark, fg = c.bg_dark }
+        on_colors = function(colors)
+          colors.bg = '#080808'
+          colors.bg_dark = '#080808'
+          colors.bg_float = '#080808'
+          colors.bg_sidebar = '#080808'
         end,
       }
     end,
