9aa3d0a111bf91e3014f0b44c467c3d085e62caf
Pawel Borak <borak.pawel@gmail.com>
2023-04-06 19:31:09 +0200

    chore: always display numbers
    This prevents layout shifting.

--- .config/nvim/lua/util/status.lua
+++ .config/nvim/lua/util/status.lua
@@ -20,11 +20,7 @@ function M.column()
     end
   end
 
-  local nu = ' '
-  local number = vim.api.nvim_win_get_option(vim.g.statusline_winid, 'number')
-  if number and vim.wo.relativenumber and vim.v.virtnum == 0 then
-    nu = vim.v.relnum == 0 and vim.v.lnum or vim.v.relnum
-  end
+  local nu = vim.v.relnum == 0 and vim.v.lnum or vim.v.relnum
   local components = {
     sign and ('%#' .. sign.texthl .. '#' .. sign.text .. '%*') or '  ',
     [[%=]],
