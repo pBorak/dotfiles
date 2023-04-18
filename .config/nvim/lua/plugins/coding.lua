2340670b0ab9ab27a0b128b067c6fccbbf7f4153
Pawel Borak <borak.pawel@gmail.com>
2023-04-18 09:51:09 +0200

    chore(cmp): remove cmdline

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -73,19 +73,18 @@ return {
   {
     'hrsh7th/nvim-cmp',
     version = false, -- last release is way too old
-    event = { 'InsertEnter', 'CmdlineEnter' },
+    event = { 'InsertEnter' },
     dependencies = {
       'hrsh7th/cmp-nvim-lsp',
-      'hrsh7th/cmp-cmdline',
       'hrsh7th/cmp-path',
       'hrsh7th/cmp-buffer',
       'saadparwaiz1/cmp_luasnip',
       'lukas-reineke/cmp-rg',
     },
-    config = function()
+    opts = function()
       local cmp = require('cmp')
 
-      cmp.setup({
+      return {
         window = {
           completion = cmp.config.window.bordered(),
           documentation = cmp.config.window.bordered(),
@@ -127,22 +126,7 @@ return {
             max_item_count = 5,
           },
         }),
-      })
-
-      cmp.setup.cmdline({ '/', '?' }, {
-        mapping = cmp.mapping.preset.cmdline(),
-        sources = {
-          { name = 'buffer' },
-        },
-      })
-
-      cmp.setup.cmdline(':', {
-        mapping = cmp.mapping.preset.cmdline(),
-        sources = cmp.config.sources({
-          { name = 'path' },
-          { name = 'cmdline', keyword_length = 2 },
-        }),
-      })
+      }
     end,
   },
 
