a9764e95a892c654f479882cebf7e877c3e26eca
Pawel Borak <borak.pawel@gmail.com>
2023-04-21 10:26:13 +0200

    fix: status

--- .config/nvim/lua/util/status.lua
+++ .config/nvim/lua/util/status.lua
@@ -2,8 +2,8 @@ local M = {}
 _G.Status = M
 
 ---@return {name:string, text:string, texthl:string}[]
-function M.get_signs()
-  local buf = vim.api.nvim_win_get_buf(vim.g.statusline_winid)
+function M.get_signs(win)
+  local buf = vim.api.nvim_win_get_buf(win)
   return vim.tbl_map(
     function(sign) return vim.fn.sign_getdefined(sign.name)[1] end,
     vim.fn.sign_getplaced(buf, { group = '*', lnum = vim.v.lnum })[1].signs
@@ -11,8 +11,9 @@ function M.get_signs()
 end
 
 function M.column()
+  local win = vim.g.statusline_winid
   local sign, git_sign
-  for _, s in ipairs(M.get_signs()) do
+  for _, s in ipairs(M.get_signs(win)) do
     if s.name:find('GitSign') then
       git_sign = s
     else
@@ -20,9 +21,12 @@ function M.column()
     end
   end
 
-  local nu = vim.v.relnum == 0 and vim.v.lnum or vim.v.relnum
+  local nu = ' '
+  if vim.wo[win].number and vim.wo[win].relativenumber and vim.v.virtnum == 0 then
+    nu = vim.v.relnum == 0 and vim.v.lnum or vim.v.relnum
+  end
   local components = {
-    sign and ('%#' .. sign.texthl .. '#' .. sign.text .. '%*') or '  ',
+    sign and ('%#' .. (sign.texthl or 'DiagnosticInfo') .. '#' .. sign.text .. '%*') or '  ',
     [[%=]],
     nu .. ' ',
     git_sign and ('%#' .. git_sign.texthl .. '#' .. git_sign.text .. '%*') or '  ',
