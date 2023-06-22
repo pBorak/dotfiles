bcde2a201eb00e077c696b8ecb5e450cb6152b16
Pawel Borak <borak.pawel@gmail.com>
2023-06-22 15:23:45 +0200

    fix(flash.nvim): remove ; and , from keymaps list

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -114,7 +114,13 @@ return {
   {
     'folke/flash.nvim',
     event = 'VeryLazy',
-    opts = {},
+    opts = {
+      modes = {
+        char = {
+          keys = { 'f', 'F', 't', 'T' },
+        },
+      },
+    },
     keys = {
       {
         's',
