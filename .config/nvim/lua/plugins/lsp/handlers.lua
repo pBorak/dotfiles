2b21fce6007773a02660cf860fcee67274a31c0f
Pawel Borak <borak.pawel@gmail.com>
2024-02-07 15:39:13 +0100

    feat: disable annoying definitions from cache/Typescript

--- .config/nvim/lua/plugins/lsp/handlers.lua
+++ .config/nvim/lua/plugins/lsp/handlers.lua
@@ -1,4 +1,6 @@
 local M = {}
+local baseDefinitionHandler = vim.lsp.handlers['textDocument/definition']
+local filter = require('util.init').filter
 
 function M.setup()
   local max_width = math.max(math.floor(vim.o.columns * 0.7), 100)
@@ -14,6 +16,24 @@ function M.setup()
     max_width = max_width,
     max_height = max_height,
   })
+end
+
+local function filter_react_dts(value)
+  -- Depending on typescript version either uri or targetUri is returned
+  if value.uri then
+    return string.match(value.uri, '%.d.ts') == nil
+  elseif value.targetUri then
+    return string.match(value.targetUri, '%.d.ts') == nil
+  end
+end
+
+function M.filter_definition_react_dtls_handler(err, result, method, ...)
+  if vim.tbl_islist(result) and #result > 1 then
+    local filtered_result = filter(result, filter_react_dts)
+    return baseDefinitionHandler(err, filtered_result, method, ...)
+  end
+
+  baseDefinitionHandler(err, result, method, ...)
 end
 
 return M
--- .config/nvim/lua/plugins/lsp/init.lua
+++ .config/nvim/lua/plugins/lsp/init.lua
@@ -102,19 +102,29 @@ return {
     'pmizio/typescript-tools.nvim',
     lazy = false,
     dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
-    opts = {
-      settings = {
-        tsserver_file_preferences = {
-          includeInlayParameterNameHints = 'literal',
-          includeInlayParameterNameHintsWhenArgumentMatchesName = false,
-          includeInlayFunctionParameterTypeHints = true,
-          includeInlayVariableTypeHints = false,
-          includeInlayPropertyDeclarationTypeHints = true,
-          includeInlayFunctionLikeReturnTypeHints = true,
-          includeInlayEnumMemberValueHints = true,
+    config = function()
+      local definition_handler =
+        require('plugins.lsp.handlers').filter_definition_react_dtls_handler
+
+      local handlers = {
+        ['textDocument/definition'] = definition_handler,
+      }
+
+      require('typescript-tools').setup({
+        handlers = handlers,
+        settings = {
+          tsserver_file_preferences = {
+            includeInlayParameterNameHints = 'literal',
+            includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+            includeInlayFunctionParameterTypeHints = true,
+            includeInlayVariableTypeHints = false,
+            includeInlayPropertyDeclarationTypeHints = true,
+            includeInlayFunctionLikeReturnTypeHints = true,
+            includeInlayEnumMemberValueHints = true,
+          },
         },
-      },
-    },
+      })
+    end,
   },
 
   {
