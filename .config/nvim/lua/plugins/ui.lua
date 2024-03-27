04a49389e845b8572c7a49139bf33012e691ff7b
Pawel Borak <borak.pawel@gmail.com>
2024-03-27 22:00:30 +0100

    chore: update notify config

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -5,17 +5,13 @@ return {
     config = function()
       local notify = require('notify')
       notify.setup({
-        render = function(...)
-          local notif = select(2, ...)
-          local style = notif.title[1] == '' and 'minimal' or 'default'
-          require('notify.render')[style](...)
-        end,
+        stages = 'static',
+        timeout = 3000,
+        max_height = function() return math.floor(vim.o.lines * 0.75) end,
+        max_width = function() return math.floor(vim.o.columns * 0.75) end,
+        on_open = function(win) vim.api.nvim_win_set_config(win, { zindex = 100 }) end,
       })
-      vim.notify = function(msg, level, opts)
-        if msg:match('Format request failed, no matching language servers.') then return end
-
-        notify(msg, level, opts)
-      end
+      vim.notify = notify
 
       vim.keymap.set('n', '<leader>N', notify.dismiss, { silent = true })
     end,
