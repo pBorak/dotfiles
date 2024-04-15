a28c7042b836a7f9904fed4c579945f7f29f29b5
Pawel Borak <borak.pawel@gmail.com>
2024-04-15 10:35:08 +0200

    feat: add textobjects to mini.ai

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -198,6 +198,16 @@ return {
         goto_left = '',
         goto_right = '',
       },
+      custom_textobjects = {
+        g = function()
+          local from = { line = 1, col = 1 }
+          local to = {
+            line = vim.fn.line('$'),
+            col = math.max(vim.fn.getline('$'):len(), 1),
+          }
+          return { from = from, to = to }
+        end,
+      },
     },
   },
 
