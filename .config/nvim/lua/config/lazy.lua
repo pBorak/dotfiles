39dc497ba61fbac9003fca33056968326c2af9fd
Pawel Borak <borak.pawel@gmail.com>
2024-05-12 11:35:10 +0200

    refactor!(colorscheme): migrate tokyonight to catppuccin

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -15,7 +15,7 @@ require('lazy').setup({
   spec = {
     { import = 'plugins' },
   },
-  install = { colorscheme = { 'tokyonight' } },
+  install = { colorscheme = { 'catppuccin' } },
   performance = {
     rtp = {
       disabled_plugins = {
