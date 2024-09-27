3f1ceee5bf17fb8a2b90678c15bf1234bc4b1054
Pawel Borak <borak.pawel@gmail.com>
2024-09-27 22:01:19 +0200

    feat: trial using c-y instead of <CR> for completion

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -122,7 +122,7 @@ return {
           ['<C-d>'] = cmp.mapping.scroll_docs(-4),
           ['<C-f>'] = cmp.mapping.scroll_docs(4),
           ['<C-e>'] = cmp.mapping.abort(),
-          ['<CR>'] = cmp.mapping.confirm({
+          ['<c-y>'] = cmp.mapping.confirm({
             select = true,
           }),
         },
