c5e90434b4c72460d70dcf6a18c9eac359c53553
Pawel Borak <borak.pawel@gmail.com>
2024-10-30 13:36:34 +0100

    Revert "feat(colorscheme): migrate from tokyonight to moonfly"
    This reverts commit 72a914047632d7ce33e63af1cf87dd0b782d3210.

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -15,7 +15,7 @@ require('lazy').setup({
   spec = {
     { import = 'plugins' },
   },
-  install = { colorscheme = { 'moonfly' } },
+  install = { colorscheme = { 'tokyonight' } },
   performance = {
     rtp = {
       disabled_plugins = {
