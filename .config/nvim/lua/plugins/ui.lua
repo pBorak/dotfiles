e0ec5ecb3fcdb29cda0a9d8be293ae89d5ffe773
Pawel Borak <borak.pawel@gmail.com>
2024-12-10 16:47:15 +0100

    refactor: migrate from gitsigns to mini.diff

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -18,7 +18,7 @@ return {
           lualine_a = { 'mode' },
           lualine_b = {
             {
-              'b:gitsigns_head',
+              'branch',
               icon = icons.misc.git_branch,
               color = {
                 gui = 'bold',
