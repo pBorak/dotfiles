b3217320119b63e67b9df39c96fc4bcb73eab6b1
Pawel Borak <borak.pawel@gmail.com>
2025-02-17 18:27:57 +0100

    Revert "chore: migrate from tokyonight to moonfly"
    This reverts commit 7866be010ec10f3d60e2de2b3418b05cf1b59a38.

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,4 +1,20 @@
 return {
 
-  { 'bluz71/vim-moonfly-colors', name = 'moonfly', lazy = false, priority = 1000 },
+  {
+    'folke/tokyonight.nvim',
+    lazy = false,
+    priority = 1000,
+    opts = function()
+      vim.api.nvim_set_hl(0, '@label.ruby', { bold = true, fg = '#4fd6be' })
+
+      return {
+        on_colors = function(colors)
+          colors.bg = '#080808'
+          colors.bg_dark = '#080808'
+          colors.bg_float = '#080808'
+          colors.bg_sidebar = '#080808'
+        end,
+      }
+    end,
+  },
 }
