68db05fa9ed42e31abfed5cb88a71a8832215d5f
Pawel Borak <borak.pawel@gmail.com>
2025-06-30 10:53:12 +0200

    feat: add formatters

--- .config/nvim/lua/plugins/lsp/init.lua
+++ .config/nvim/lua/plugins/lsp/init.lua
@@ -81,6 +81,10 @@ return {
     opts = {
       formatters_by_ft = {
         lua = { 'stylua' },
+        javascript = { 'prettier' },
+        javascriptreact = { 'prettier' },
+        typescript = { 'prettier' },
+        typescriptreact = { 'prettier' },
       },
     },
     init = function() vim.o.formatexpr = "v:lua.require'conform'.formatexpr()" end,
