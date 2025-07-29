23d7fee5501e623eb055f714a19053976278045a
Pawel Borak <borak.pawel@gmail.com>
2025-07-29 12:13:39 +0200

    feat: migrate to blink.cmp again

--- .config/nvim/lua/plugins/lsp/init.lua
+++ .config/nvim/lua/plugins/lsp/init.lua
@@ -5,7 +5,7 @@ return {
     event = 'BufReadPre',
     dependencies = {
       'mason.nvim',
-      'hrsh7th/cmp-nvim-lsp',
+      'saghen/blink.cmp',
     },
     opts = function()
       return {
@@ -42,7 +42,7 @@ return {
 
       local servers = opts.servers
       local capabilities =
-        require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
+        require('blink.cmp').get_lsp_capabilities(vim.lsp.protocol.make_client_capabilities())
 
       local function setup(server)
         local server_opts = vim.tbl_deep_extend('force', {
